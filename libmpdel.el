e23bc52117b7adec3b1c2406fcd4092ab0e6593d
Damien Cassou <damien@cassou.me>
2019-09-18 18:07:36 +0200

    Release 1.1.1

--- libmpdel.el
+++ libmpdel.el
@@ -6,7 +6,7 @@
 ;; Keywords: multimedia
 ;; Url: https://gitlab.petton.fr/mpdel/libmpdel
 ;; Package-requires: ((emacs "25.1"))
-;; Version: 1.1.0
+;; Version: 1.1.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
